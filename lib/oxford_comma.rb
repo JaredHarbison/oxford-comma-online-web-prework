76d6c8c6a7993f73ba761156da176fda3f3d6d33
Jared Harbison <jared.harbison@gmail.com>
2018-12-31 06:25:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2 
     return array.join (" and ")
   else array.length > 2 
-    array[-1].prepend "and ".join (", ")
+    array[-1].prepend "and "
+    array.join(", ")
   end
 end
